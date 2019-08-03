4964a2ad823c68f7fe992d343c1ac25d2db2c738
rsp <1747836307@qq.com>
2019-08-03 15:56:01 +0800

    update for the first time, some demo and pictures.

---
+++ cartoon.h
@@ -0,0 +1,5 @@
+//include OpenCV's C++ Interface
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
--- main_desktop.cpp
+++ main_desktop.cpp
@@ -14,6 +14,28 @@ if (!camera.isOpened()){
 }
 
 //try to set the camera resolution;
+camera.set(cv::CV_CAP_PROP_FRAME_WIDTH, 640);
+camera.set(cv::CV_CAP_PROP_FRAME_HEIGHT, 480);
 
-camera.set(cv::CV_CAP_PROP_FRAME_WIDTH,640);
-camera.set(cv::CV_CAP_PROP)
+while(true){
+	//grab the next camera frame.
+	cv::Mat cameraFrame;
+	camera >> cameraFrame;
+	if(cameraFrame.empty()){
+		cerr<<"Error: couldn't grab a camera frame." << endl;
+		exit(1);
+	}
+	//create a blank output image, that we will draw onto.
+	cv::Mat displayedFrame(cameraFrame.size(), cv::CV_8UC3);
+
+	//Run the cartoonifier filter on the camera frame.
+	cartoonifyImage(cameraFrame, displayedFrame);
+	
+	//Display the processed image onto the screen.
+	imshow("Cartoonifier", displayedFrame);
+
+	char keypress = cv::waitKey(20); //Need this to see anything!
+	if(keypress == 27){
+		break;
+	}
+}
